eabb14e00e126c25e1dd5d6837805fdadce79178
Francis Gonzalez <Kitomc.rd@gmail.com>
2019-06-20 11:42:40 +0800

    Agregar archivos de proyecto.

---
+++ Capa_Dato/Datos_Factura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Capa_Dato
+{
+  public class Datos_Factura
+    {
+        Sistema_FacturacionEntities modeldb = new Sistema_FacturacionEntities();
+
+
+    }
+}
---
+++ Capa_Dato/Entidad_Factura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Dato
+{
+   public class Entidad_Factura
+    {
+
+
+        public int ID_Fctura { get; set; }
+        public int ID_Cliente { get; set; }
+        public int ID_Producto { get; set; }
+        public float Total { get; set; }
+        public System.DateTime Fecha { get; set; }
+        public int Cantidad { get; set; }
+        public float Subtotal { get; set; }
+
+    }
+}
